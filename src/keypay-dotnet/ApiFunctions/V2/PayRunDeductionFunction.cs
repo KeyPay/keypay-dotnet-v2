a4d52ab0963b29d62d4d5374fc05cd58e1211260
Ken Nguyen <ken@webscale.com.au>
2017-06-06 18:21:58 +1000

    Add function to get deductions from a payrun for a specific employee

--- src/keypay-dotnet/ApiFunctions/V2/PayRunDeductionFunction.cs
+++ src/keypay-dotnet/ApiFunctions/V2/PayRunDeductionFunction.cs
@@ -15,6 +15,11 @@ namespace KeyPay.ApiFunctions.V2
             return ApiRequest<DeductionsResponse>("/business/" + businessId + "/payrun/" + payRunId + "/deductions");
         }
 
+        public DeductionsResponse List(int businessId, int payRunId, int employeeId)
+        {
+            return ApiRequest<DeductionsResponse>("/business/" + businessId + "/payrun/" + payRunId + "/deductions/" + employeeId);
+        }
+
         public DeductionModel Submit(int businessId, SubmitDeductionsRequest deductions)
         {
             return ApiRequest<DeductionModel, SubmitDeductionsRequest>("/business/" + businessId + "/payrun/" + deductions.PayRunId + "/deductions", deductions, Method.POST);            
