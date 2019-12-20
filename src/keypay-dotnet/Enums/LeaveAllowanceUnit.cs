1c80e1db711b767c28c8a0a3a4ca636372b16324
Ken Nguyen <ken@webscale.com.au>
2019-12-20 14:27:38 +1100

    update client so that it can return unit-based leave categories

--- src/keypay-dotnet/Enums/LeaveAllowanceUnit.cs
+++ src/keypay-dotnet/Enums/LeaveAllowanceUnit.cs
@@ -5,6 +5,8 @@
         Days = 1,
         Weeks = 2,
         HoursPerHourWorked = 3,
-        HoursPerPayRun = 4
+        HoursPerPayRun = 4,
+        StandardDays = 5,
+        StandardWeeks = 6,
     }
 }
