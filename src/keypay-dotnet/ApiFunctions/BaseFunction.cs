b04bc54c543bec8fe8302c3a94bb45053e84a838
Paul Duran <paul@webscale.com.au>
2020-04-09 13:10:52 +1000

    sanitise URLs to remove stray '&' from the end as newer restsharps aren't tolerant like they used to be

--- src/keypay-dotnet/ApiFunctions/BaseFunction.cs
+++ src/keypay-dotnet/ApiFunctions/BaseFunction.cs
@@ -14,22 +14,24 @@ namespace KeyPay.ApiFunctions
             Api = api;
         }
 
+        private string SanitiseUrl(string url) => url.EndsWith("&") ? url.Remove(url.Length - 1, 1) : url;
+
         protected void ApiRequest(string url, Method method = Method.GET)
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             Api.Execute(req);
         }
 
         protected T ApiRequest<T>(string url, Method method = Method.GET) where T : new()
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             var result = Api.Execute<T>(req);
             return result;
         }
 
         protected TResult ApiRequest<TResult, TInput>(string url, TInput input, Method method = Method.GET) where TResult : new()
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             AddParameters(req, method, input);
             var result = Api.Execute<TResult>(req);
             return result;
@@ -49,14 +51,14 @@ namespace KeyPay.ApiFunctions
 
         protected void ApiRequest(string url, object input, Method method = Method.GET)
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             AddParameters(req, method, input);
             Api.Execute(req);
         }
         
         protected T ApiFileRequest<T>(string url, FileUploadModel file, Method method = Method.POST) where T : new()
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             req.AddFile(file.FileName, file.File, file.FileName);
             var result = Api.Execute<T>(req);
             return result;
@@ -64,7 +66,7 @@ namespace KeyPay.ApiFunctions
 
         protected T ApiFileRequest<T>(string url, List<FileUploadModel> files, Method method = Method.POST) where T : new()
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             foreach (var file in files)
             {
                 req.AddFile(file.FileName, file.File, file.FileName);
@@ -75,28 +77,28 @@ namespace KeyPay.ApiFunctions
 
         protected T ApiByteArrayRequest<T>(string url, Method method = Method.GET) where T : new()
         {
-            var req = new RestRequest(url, method);
+            var req = new RestRequest(SanitiseUrl(url), method);
             var response = Api.Execute(req);
             var result = JsonConvert.DeserializeObject<T>(response);
             return result;
         }
         protected byte[] ApiByteArrayRequest(string url, Method method = Method.GET)
         {
-            var req = new RestRequest(url, method);
+            var req = new RestRequest(SanitiseUrl(url), method);
             var result = Api.DownloadFile(req);
             return result;
         }
 
         protected string ApiJsonRequest(string url, Method method = Method.GET)
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             var result = Api.Execute(req);
             return result;
         }
 
         protected T ApiJsonRequest<T>(string url, Method method = Method.GET)
         {
-            var req = new RestRequest(url, method) {JsonSerializer = new CustomSerializer()};
+            var req = new RestRequest(SanitiseUrl(url), method) {JsonSerializer = new CustomSerializer()};
             var response = Api.Execute(req);
             var result = JsonConvert.DeserializeObject<T>(response);
             return result;
