5a64595ab0c8271d3621189f6553a4d24acfbea3
ci <ci@webscale.com.au>
2025-05-06 10:45:44 +0000

    Build 3.0.0.68 updates

--- src/keypay-dotnet/Sg/Models/PayRun/SgPayRunDetailsModel.cs
+++ src/keypay-dotnet/Sg/Models/PayRun/SgPayRunDetailsModel.cs
@@ -11,5 +11,15 @@ namespace KeyPayV2.Sg.Models.PayRun
         public IList<SgPayRunTotalDetailModel> PayRunTotals { get; set; }
         public SgPayRunGrandTotalModel GrandTotal { get; set; }
         public PayRunModel PayRun { get; set; }
+        public int Id { get; set; }
+        public DateTime? DateFinalised { get; set; }
+        public int PayScheduleId { get; set; }
+        public DateTime PayPeriodStarting { get; set; }
+        public DateTime PayPeriodEnding { get; set; }
+        public DateTime DatePaid { get; set; }
+        public bool IsFinalised { get; set; }
+        public bool PaySlipsPublished { get; set; }
+        public string Notation { get; set; }
+        public string ExternalId { get; set; }
     }
 }
--- src/keypay-dotnet/Uk/Models/PayRun/UkPayRunDetailsModel.cs
+++ src/keypay-dotnet/Uk/Models/PayRun/UkPayRunDetailsModel.cs
@@ -11,5 +11,15 @@ namespace KeyPayV2.Uk.Models.PayRun
         public IList<UkPayRunTotalDetailModel> PayRunTotals { get; set; }
         public UkPayRunGrandTotalModel GrandTotal { get; set; }
         public PayRunModel PayRun { get; set; }
+        public int Id { get; set; }
+        public DateTime? DateFinalised { get; set; }
+        public int PayScheduleId { get; set; }
+        public DateTime PayPeriodStarting { get; set; }
+        public DateTime PayPeriodEnding { get; set; }
+        public DateTime DatePaid { get; set; }
+        public bool IsFinalised { get; set; }
+        public bool PaySlipsPublished { get; set; }
+        public string Notation { get; set; }
+        public string ExternalId { get; set; }
     }
 }
