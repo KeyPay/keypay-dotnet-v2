5caa64c9f05decfbc222ec3d8a7df2c8fb960587
ci <ci@webscale.com.au>
2025-10-14 13:28:08 +0000

    Build 3.0.0.331 updates

--- src/keypay-dotnet/My/Enums/ExternalService.cs
+++ src/keypay-dotnet/My/Enums/ExternalService.cs
@@ -5,6 +5,7 @@ namespace KeyPayV2.My.Enums
 {
     public enum ExternalService
     {
+        Harmony,
         None,
         Saasu,
         Xero,
@@ -16,7 +17,6 @@ namespace KeyPayV2.My.Enums
         FileImport,
         FileExport,
         QuickBooks,
-        Harmony,
         AwardStore,
         Attache,
         IntegratedRostering,
