c43793a80277a1acf877df8855303d52f18cb86e
ci <ci@webscale.com.au>
2025-11-17 14:37:14 +0000

    Build 3.0.0.396 updates

--- src/keypay-dotnet/Uk/Models/Business/HmrcSettingsResponseModel.cs
+++ src/keypay-dotnet/Uk/Models/Business/HmrcSettingsResponseModel.cs
@@ -20,6 +20,7 @@ namespace KeyPayV2.Uk.Models.Business
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
         public string Suburb { get; set; }
+        public string Postcode { get; set; }
         public string Country { get; set; }
         public bool IsRtiEnabled { get; set; }
         public bool RtiTestMode { get; set; }
