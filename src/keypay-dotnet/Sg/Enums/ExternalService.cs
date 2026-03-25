879f426219d477d0a1a2f3b51aa4987040cf2a53
ci <ci@webscale.com.au>
2026-03-25 21:00:37 +0000

    Build 3.0.0.622 updates

--- src/keypay-dotnet/Sg/Enums/ExternalService.cs
+++ src/keypay-dotnet/Sg/Enums/ExternalService.cs
@@ -5,9 +5,9 @@ namespace KeyPayV2.Sg.Enums
 {
     public enum ExternalService
     {
-        Xero,
         None,
         Saasu,
+        Xero,
         MYOB,
         Deputy,
         EmployeeTimePunch,
