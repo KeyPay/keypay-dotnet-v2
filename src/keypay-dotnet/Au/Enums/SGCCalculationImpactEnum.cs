ac5cf3b73c5a2ae87f789a4798571f453724d24c
ci <ci@webscale.com.au>
2026-07-29 01:37:48 +0000

    Build 3.0.0.884 updates

--- src/keypay-dotnet/Au/Enums/SGCCalculationImpactEnum.cs
+++ src/keypay-dotnet/Au/Enums/SGCCalculationImpactEnum.cs
@@ -6,6 +6,7 @@ namespace KeyPayV2.Au.Enums
     public enum SGCCalculationImpactEnum
     {
         None,
-        ReducesOTE
+        ReducesOTE,
+        ReducesQeOnly
     }
 }
--- src/keypay-dotnet/Uk/Enums/SGCCalculationImpactEnum.cs
+++ src/keypay-dotnet/Uk/Enums/SGCCalculationImpactEnum.cs
@@ -6,6 +6,7 @@ namespace KeyPayV2.Uk.Enums
     public enum SGCCalculationImpactEnum
     {
         None,
-        ReducesOTE
+        ReducesOTE,
+        ReducesQeOnly
     }
 }
