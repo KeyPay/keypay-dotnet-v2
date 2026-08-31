98c91afc99bf1086f894b45654bd0ae764f715c6
ci <ci@webscale.com.au>
2026-08-31 02:03:12 +0000

    Build 3.0.0.963 updates

--- src/keypay-dotnet/Au/Models/Reporting/AuCostingReportApiModel.cs
+++ src/keypay-dotnet/Au/Models/Reporting/AuCostingReportApiModel.cs
@@ -9,6 +9,8 @@ namespace KeyPayV2.Au.Models.Reporting
     public class AuCostingReportApiModel
     {
         public decimal SuperContribution { get; set; }
+        public decimal EmployerContribution { get; set; }
+        public decimal NonRescEmployerContribution { get; set; }
         public int LocationId { get; set; }
         public string LocationName { get; set; }
         public IDictionary<String,Decimal> PayCategories { get; set; }
